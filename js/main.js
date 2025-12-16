e82c85b52cb9987ed273325e9bb400a0c1b42bd1
Abdo3638 <Abdoelgonimy@gmail.com>
2025-12-16 23:47:12 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -52,20 +52,26 @@ function countingOnscroll(arr, isPercentage) {
 // The end of the year
 let countDownDate = new Date("Dec 31,2027 23:59:59").getTime();
 let counter = setInterval(() => {
-    //get date now
-    let dateNow = new Date().getTime();
-    // find the differance between Now & Countdown Date
-    let differance = countDownDate - dateNow;
-    // get time units
-    let days = Math.floor(differance / (1000 * 60 * 60 * 24)) ;
-    let hours = Math.floor((differance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) ;
-    let minutes = Math.floor((differance % (1000 * 60 * 60)) / (1000 * 60)); 
-    let seconds = Math.floor((differance % (1000 * 60)) / 1000); 
-    document.querySelector(".days-span").innerHTML = days < 10 ? `0${days}`: days;
-    document.querySelector(".hours-span").innerHTML = hours < 10 ? `0${hours}`: hours;
-    document.querySelector(".minutes-span").innerHTML = minutes < 10 ? `0${minutes}`: minutes;
-    document.querySelector(".seconds-span").innerHTML = seconds < 10 ? `0${seconds}`: seconds;
-    if(differance == 0){
-        clearInterval(counter);
-    }
+  //get date now
+  let dateNow = new Date().getTime();
+  // find the differance between Now & Countdown Date
+  let differance = countDownDate - dateNow;
+  // get time units
+  let days = Math.floor(differance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (differance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((differance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((differance % (1000 * 60)) / 1000);
+  document.querySelector(".days-span").innerHTML =
+    days < 10 ? `0${days}` : days;
+  document.querySelector(".hours-span").innerHTML =
+    hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes-span").innerHTML =
+    minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds-span").innerHTML =
+    seconds < 10 ? `0${seconds}` : seconds;
+  if (differance == 0) {
+    clearInterval(counter);
+  }
 }, 1000);
